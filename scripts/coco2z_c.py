9486d69f430a65fcd92e4e1cdbd4f52bb0bc2025
Reese Kneeland <reesekneeland@gmail.com>
2023-01-17 14:49:15 -0600

    updating tester scripts

--- scripts/coco2z_c.py
+++ scripts/coco2z_c.py
@@ -46,11 +46,14 @@ def load_model_from_config(config, ckpt, verbose=False):
     model.eval()
     return model
 
-#takes in numpy array of image from nsd hdf5 file
+# Takes in numpy array of image from nsd hdf5 file
 def load_img(im_array):
+
     image = Image.fromarray(im_array)
     w, h = image.size
+
     # print(f"loaded input image of size ({w}, {h}) from array")
+
     w, h = 512, 512  # resize to integer multiple of 64
     imagePil = image.resize((w, h), resample=Image.Resampling.LANCZOS)
     image = np.array(imagePil).astype(np.float32) / 255.0
@@ -76,60 +79,89 @@ def main():
         default="configs/stable-diffusion/v2-inference.yaml",
         help="path to config which constructs model",
     )
+    
     parser.add_argument(
         "--ckpt",
         type=str,
         help="path to checkpoint of model",
     )
 
+    # First URL: This is the original read-only NSD file path (The actual data)
+    # Second URL: Local files that we are adding to the dataset and need to access as part of the data
+    # Object for the NSDAccess package
     nsda = NSDAccess('/home/naxos2-raid25/kneel027/home/surly/raid4/kendrick-data/nsd', '/home/naxos2-raid25/kneel027/home/kneel027/nsd_local')
 
+    # Setting up the device to use the GPU
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    # Sets the default parameters for LDM
     opt = parser.parse_args()
+
+    # Loading the default stable diffusion model. 
+    # This is the configuration for the stable diffusion model.
     config = OmegaConf.load(f"{opt.config}")
-    
     model = load_model_from_config(config, f"{opt.ckpt}")
     model = model.to(device)
+    
+    # Initialize OpenClip model
     openclip_model, _, oc_preprocess = open_clip.create_model_and_transforms('ViT-H-14', pretrained='laion2b_s32b_b79k', device=device)
     
-    #create dataframe of all nsd images and their associated z and c tensors
-    # df = pd.DataFrame({'id': [], 'img': [], 'promptList': [], 'bestPrompt': [], 'z': [], 'c': []})
-    
-    #iterate through all images and captions in nsd sampled from COCO
+    # Importing the caption object with 5 captions per image
+    # Captions is a lists of lists, containing the 5 captions. 
     captions = nsda.read_image_coco_info([i for i in range(73000)], info_type='captions', show_annot=False)
+    
+    # Iterate through all images and captions in nsd sampled from COCO
     for i in range(0, 73000):
+        
+        # Array of image data 1 x 425 x 425 x 3 (Stores pixel intensities)
         img = nsda.read_images([i], show=False)
+        
+        # Variable to store the captions of the image. 
         prompts = []
+        
+        # Load the 5 prompts for each image
         for j in range(len(captions[i])):
+
+            # Index into the caption list and get the corresponding 5 captions. 
             prompts.append(captions[i][j]['caption'])
         
+        # Converting images to the format openClip likes
         init_image, img_pil = load_img(img[0])
         init_image = repeat(init_image, '1 ... -> b ...', b=1)
         image = oc_preprocess(img_pil).unsqueeze(0).to(device)
+        
+        # Tokenize the image 1 x 77
         token = open_clip.tokenize(prompts).to(device)
         
-        # move to latent space to create latent z vector
-        z = model.get_first_stage_encoding(model.encode_first_stage(init_image.to(device))) 
+        # Generate clip embeddings for each prompt to find the best one
+        img_embed = openclip_model.encode_image(image) # 1 x 1024
+        text_embed = openclip_model.encode_text(token) # 1 x 1024
         
-        #generate clip embeddings for each prompt to find the best one
-        img_embed = openclip_model.encode_image(image)
-        text_embed = openclip_model.encode_text(token)
+        # Normalize the embeddings so that we can calculate probability
         img_embed_norm = img_embed/img_embed.norm(dim=-1, keepdim=True)
         text_embed_norm = text_embed/text_embed.norm(dim=-1, keepdim=True)
+        
+        # Calculate the probability of a particular text embedding matching each image
         probs = (100.0 * img_embed_norm @ text_embed_norm.T).softmax(dim=-1)
+        
+        # Pick a singular prompt thats the best
         prompt = prompts[torch.argmax(probs)]
-        print(prompt)
-        #get expanded clip embedding for best prompt
+
+        # Get expanded clip embedding (1 x 77 x 1024) for best prompt using Stable Diffusion methods
         precision_scope = autocast
         with torch.no_grad():
             with precision_scope("cuda"):
                 with model.ema_scope():
-                    c = model.get_learned_conditioning(prompt)
-        print("SAVING ", i)
+
+                    # Stable diffusion encoder. Converts the text into a stable diffusion encoded clip vector. 
+                    c = model.get_learned_conditioning(prompt) #1x77x1024
+                    
+        # Use the Stable Diffusion method to move to latent space to create latent z vector
+        z = model.get_first_stage_encoding(model.encode_first_stage(init_image.to(device))) # 1 x 4 x 64 x 64
+        
+        # Save the c and z vectors into there corresponding files. 
         torch.save(c, "/home/naxos2-raid25/kneel027/home/kneel027/nsd_local/nsddata_stimuli/tensors/c/" + str(i) + ".pt")
         torch.save(z, "/home/naxos2-raid25/kneel027/home/kneel027/nsd_local/nsddata_stimuli/tensors/z/" + str(i) + ".pt")
-    #     df = pd.concat([df, pd.DataFrame({id: i, 'img': img, 'promptList': prompts, 'bestPrompt': prompt, 'z': z.cpu().numpy(), 'c': c.cpu().numpy()})], axis=0, join='outer', ignore_index=True)
-    # df.to_hdf('z_c_tensors/10.hdf','df', mode='w')
 
 
 if __name__ == "__main__":
